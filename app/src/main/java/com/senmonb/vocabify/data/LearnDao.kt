dbc205c62628a9ec0126713796ea7d680db765c6
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-18 11:27:11 +0900

    add LearnDao

---
+++ app/src/main/java/com/senmonb/vocabify/data/LearnDao.kt
@@ -0,0 +1,30 @@
+package com.senmonb.vocabify.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LearnDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(learn: Learn)
+
+    @Update
+    suspend fun update(learn: Learn)
+
+    @Delete
+    suspend fun delete(learn: Learn)
+
+    @Query("SELECT * from learn")
+    fun getAllLearn(): Flow<List<Learn>>
+
+    @Query("SELECT * FROM learn WHERE is_look = 0")
+    fun getAllUnlearned(): Flow<List<Learn>>
+
+    @Query("SELECT * FROM learn WHERE is_look = 1")
+    fun getLearned(): Flow<List<Learn>>
+}
