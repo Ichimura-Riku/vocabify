92d266554bd4fb79d02d1bb4d60b538a905cf800
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-18 12:35:24 +0900

    add LearnRepositoryModules

---
+++ app/src/main/java/com/senmonb/vocabify/di/LearnRepositoryModules.kt
@@ -0,0 +1,23 @@
+package com.senmonb.vocabify.di
+
+import android.content.Context
+import com.senmonb.vocabify.data.LearnDatabase
+import com.senmonb.vocabify.data.LearnRepository
+import com.senmonb.vocabify.data.OfflineLearnRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object LearnRepositoryModules {
+    @Provides
+    @Singleton
+    fun bindLearnRepository(@ApplicationContext context: Context): LearnRepository{
+        return OfflineLearnRepository(LearnDatabase.getDatabase(context).learnDao())
+    }
+
+}
