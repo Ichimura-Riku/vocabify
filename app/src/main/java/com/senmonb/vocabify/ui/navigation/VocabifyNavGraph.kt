7f6cf13bde32700ed07cd03ee58e9a4fc74ebab6
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-19 21:36:32 +0900

    add VocabifyNavHost to VocabifyNavGraph

---
+++ app/src/main/java/com/senmonb/vocabify/ui/navigation/VocabifyNavGraph.kt
@@ -0,0 +1,23 @@
+package com.senmonb.vocabify.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.senmonb.vocabify.ui.screen.HomeDestination
+import com.senmonb.vocabify.ui.screen.HomeScreen
+
+@Composable
+fun VocabifyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(navController = navController, startDestination = HomeDestination.route, modifier = modifier) {
+        composable(route = HomeDestination.route) {
+            HomeScreen(
+                modifier = modifier,
+            )
+        }
+    }
+}
