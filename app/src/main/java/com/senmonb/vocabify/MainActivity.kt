e989ae716d44d7795292c6aa78ed314671c82dcb
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-19 19:02:11 +0900

    change setContent

--- app/src/main/java/com/senmonb/vocabify/MainActivity.kt
+++ app/src/main/java/com/senmonb/vocabify/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.senmonb.vocabify.ui.screen.HomeScreen
 import com.senmonb.vocabify.ui.theme.VocabifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,28 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VocabifyTheme {
-        Greeting("Android")
     }
 }
