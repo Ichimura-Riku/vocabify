aaf4c262ebc1417d69451fd298b0e7d1e21e70da
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-19 20:44:11 +0900

    add tempLayout to HomeScreen

--- app/src/main/java/com/senmonb/vocabify/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/senmonb/vocabify/ui/screen/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.senmonb.vocabify.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -16,7 +19,6 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
-
     Scaffold { innerPadding ->
         HomeContainer(
             modifier = modifier
@@ -32,8 +34,25 @@ fun HomeScreen(
 fun HomeContainer(
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text("Let's Learn Vocabulary")
+        Column {
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "add Vocabulary")
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "learning English-Japanese")
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "learning Japanese-English")
+            }
+        }
+    }
 }
