64bcb3c2cf8732aeadd48db9eaf60da9a3e3fa29
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-11-19 22:46:45 +0900

    add HomeViewModel(file creation only)

---
+++ app/src/main/java/com/senmonb/vocabify/ui/screen/AddVocabularyViewModel.kt
@@ -0,0 +1,10 @@
+package com.senmonb.vocabify.ui.screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddVocabularyViewModel
+    @Inject
+    constructor() : ViewModel()
